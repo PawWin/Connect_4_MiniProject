94f090005c1bc592b5d57e4086343674ec6c4db9
Paweł Winkler <147107563+PawWin@users.noreply.github.com>
2024-03-20 15:39:15 +0100

    Added a function to check for winner

--- main.py
+++ main.py
@@ -25,3 +25,33 @@ def printBoard():
 def modifyBoard(spacePicked, turn):
     Board[spacePicked[0]][spacePicked[1]] = turn
 
+def checkForWinner(chip):
+  # Check horizontal spaces
+  for y in range(rows):
+    for x in range(cols - 3):
+      if Board[x][y] == chip and Board[x+1][y] == chip and Board[x+2][y] == chip and Board[x+3][y] == chip:
+        print("\nGame over!", chip, "wins! Thank you for playing :)")
+        return True
+
+  # Check vertical spaces
+  for x in range(rows):
+    for y in range(cols - 3):
+      if Board[x][y] == chip and Board[x][y+1] == chip and Board[x][y+2] == chip and Board[x][y+3] == chip:
+        print("\nGame over!", chip, "wins! Thank you for playing :)")
+        return True
+
+  # Check upper right to bottom left diagonal spaces
+  for x in range(rows - 3):
+    for y in range(3, cols):
+      if Board[x][y] == chip and Board[x+1][y-1] == chip and Board[x+2][y-2] == chip and Board[x+3][y-3] == chip:
+        print("\nGame over!", chip, "wins! Thank you for playing :)")
+        return True
+
+  # Check upper left to bottom right diagonal spaces
+  for x in range(rows - 3):
+    for y in range(cols - 3):
+      if Board[x][y] == chip and Board[x+1][y+1] == chip and Board[x+2][y+2] == chip and Board[x+3][y+3] == chip:
+        print("\nGame over!", chip, "wins! Thank you for playing :)")
+        return True
+  return False
+
