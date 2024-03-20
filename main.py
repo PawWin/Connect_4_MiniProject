d427e1061154ffbb830ade466226b741ce16486f
Paweł Winkler <147107563+PawWin@users.noreply.github.com>
2024-03-20 15:20:24 +0100

    Added Board

---
+++ main.py
@@ -0,0 +1,24 @@
+import random as rd
+#🔵
+#🔴
+print("Welcome to Connect Four")
+print("-----------------------")
+Letters = ["A","B","C","D","E","F","G"]
+Board = [["","","","","","",""], ["","","","","","",""], ["","","","","","",""], ["","","","","","",""], ["","","","","","",""], ["","","","","","",""]]
+rows = 6
+cols = 7
+
+def printBoard():
+  print("\n     A    B    C    D    E    F    G  ", end="")
+  for x in range(rows):
+    print("\n   +----+----+----+----+----+----+----+")
+    print(x, " |", end="")
+    for y in range(cols):
+      if(Board[x][y] == "X"):
+        print("",Board[x][y], end=" |")
+      elif(Board[x][y] == "O"):
+        print("", Board[x][y], end=" |")
+      else:
+        print(" ", Board[x][y], end="  |")
+  print("\n   +----+----+----+----+----+----+----+")
+
